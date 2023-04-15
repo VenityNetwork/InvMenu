f7e0c6dcb30ed39d2a340a509d0bc61de81472d3
AkmalFairuz <akmalf475@gmail.com>
2023-04-15 11:23:12 +0700

    Reduce delay

--- src/muqsit/invmenu/session/network/PlayerNetwork.php
+++ src/muqsit/invmenu/session/network/PlayerNetwork.php
@@ -28,7 +28,7 @@ final class PlayerNetwork{
 	private Closure $container_open_callback;
 
 	private ?NetworkStackLatencyEntry $current = null;
-	private int $graphic_wait_duration = 200;
+	private int $graphic_wait_duration = 50;
 
 	/** @var SplQueue<NetworkStackLatencyEntry> */
 	private SplQueue $queue;
